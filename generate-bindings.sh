b24f29639682c10ca9b8a9745a3dc2d2a412f123
Andrew Watts <ahwatts@gmail.com>
2019-11-30 07:19:58 -0500

    Add some constants to the bdingen output.

---
+++ generate-bindings.sh
@@ -0,0 +1,7 @@
+bindgen \
+    --whitelist-function "^snd_.*" \
+    --whitelist-var "^SND_.*" \
+    --whitelist-var "^IEC958_.*" \
+    --whitelist-var "^MIDI_.*" \
+    --whitelist-type "^snd_.*" \
+    $@
